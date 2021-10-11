d6118b50591b050eb4cba8ca49d2e41428ec0ba0
bbFactory <borahm87@gmail.com>
2021-10-11 14:03:31 +0000

    Feat : Color 모델 정의

--- server/common/models.py
+++ server/common/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Color(models.Model):
+    hex_code = models.CharField(max_length=6, unique=True, verbose_name="색상 코드")
+
+    def __str__(self):
+        return f"{self.hex_code}({self.id})"
+
+    class Meta:
+        db_table = "color"
+        verbose_name = "색상"
+        verbose_name_plural = "색상"
+        ordering = ["id"]
