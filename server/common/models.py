48cb08708059af44b3893f9e0f546221386c73e9
HyunAm <tlfgjawlq@naver.com>
2021-10-16 14:50:51 +0900

    Feat : brand migrations 파일 추가
    - brand migrations 파일 추가

--- server/common/models.py
+++ server/common/models.py
@@ -11,12 +11,20 @@ class Color(BaseModel):
         return f"{self.hex_code}({self.id})"
 
     class Meta:
-        db_table = "color"
+        db_table = "common_color"
         verbose_name = "색상"
         verbose_name_plural = "색상"
         ordering = ["id"]
 
 
 class Brand(BaseModel):
-    name = models.CharField(max_length=64, blank=True, null=True)
-    
+    name = models.CharField("브랜드", max_length=64, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name}({self.id})"
+
+    class Meta:
+        db_table = "common_brand"
+        verbose_name = "색상"
+        verbose_name_plural = "색상"
+        ordering = ["id"]
