e10c1f0e74f0b5daadc090beeb5c17b3004e2b7c
bbFactory <borahm87@gmail.com>
2021-10-17 13:34:07 +0900

    cloth 및 shoppingmall 앱에 속하는 모델 추가 (#5)
    * Feat : category 모델(cloth) 추가
    
    * Feat : attribute 모델(cloth) 추가
    
    * Feat : clothes 모델(cloth) 추가
    
    * Feat : shoppingmall 모델(shoppingmall) 추가

--- server/cloth/models.py
+++ server/cloth/models.py
@@ -1,3 +1,74 @@
 from django.db import models
+from server.base.models import BaseModel
 
-# Create your models here.
+
+def image_upload_to(instance, filename):
+    return os.path.join(instance.UPLOAD_PATH, f"{filename}")
+
+
+class Clothes(BaseModel):
+    UPLOAD_PATH = "clothes"
+
+    image = models.ImageField(
+        upload_to=image_upload_to,
+        verbose_name="의류 이미지",
+    )
+    member = models.ForeignKey(
+        "member.Members",
+        null=True,
+        verbose_name="멤버",
+        on_delete=models.CASCADE
+    )
+    color = models.ForeignKey(
+        "common.Color",
+        null=True,
+        verbose_name="의류 색상",
+        on_delete=models.CASCADE
+    )
+    category = models.ForeignKey(
+        "cloth.Category",
+        null=True,
+        verbose_name="의류 카테고리",
+        on_delete=models.CASCADE
+    )
+    attributes = models.ManyToManyField(
+        "cloth.Attribute",
+        verbose_name="의류 속성",
+    )
+
+    def __str__(self):
+        return f"의류({self.id})"
+
+    class Meta:
+        db_table = "cloth_clothes"
+        verbose_name = "의류"
+        verbose_name_plural = "의류"
+        ordering = ["id"]
+
+
+class Category(models.Model):
+    ko_name = models.CharField(max_length=32, verbose_name="의류 카테고리 한국어")
+    en_name = models.CharField(max_length=32, verbose_name="의류 카테고리 영어")
+
+    def __str__(self):
+        return f"{self.en_name}({self.ko_name})"
+
+    class Meta:
+        db_table = "cloth_category"
+        verbose_name = "의류 카테고리"
+        verbose_name_plural = "의류 카테고리"
+        ordering = ["id"]
+
+
+class Attribute(models.Model):
+    ko_name = models.CharField(max_length=32, verbose_name="의류 속성 한국어")
+    en_name = models.CharField(max_length=32, unique=True, verbose_name="의류 속성 영어")
+
+    def __str__(self):
+        return f"{self.en_name}({self.ko_name})"
+
+    class Meta:
+        db_table = "cloth_attribute"
+        verbose_name = "의류 속성"
+        verbose_name_plural = "의류 속성"
+        ordering = ["id"]
--- server/shoppingmall/models.py
+++ server/shoppingmall/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from server.base.models import BaseModel
 
-# Create your models here.
+
+def image_upload_to(instance, filename):
+    return os.path.join(instance.UPLOAD_PATH, f"{filename}")
+
+
+class ShoppingMall(BaseModel):
+    UPLOAD_PATH = "shoppingmall"
+
+    image = models.ImageField(
+        upload_to=image_upload_to,
+        verbose_name="쇼핑몰 의류 이미지",
+    )
+    brand = models.ForeignKey(
+        "common.Brand",
+        null=True,
+        verbose_name="브랜드",
+        on_delete=models.CASCADE
+    )
+    cloth = models.ManyToManyField(
+        "cloth.Clothes",
+        verbose_name="의류",
+    )
+    url = models.TextField(verbose_name="웹 페이지 URL")
+    price = models.IntegerField("가격", default=0)
+
+    def __str__(self):
+        return f"쇼핑몰({self.id})"
+
+    class Meta:
+        db_table = "shoppingmall_shoppingmall"
+        verbose_name = "쇼핑몰"
+        verbose_name_plural = "쇼핑몰"
+        ordering = ["id"]
+
