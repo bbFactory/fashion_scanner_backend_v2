75a885ecb6fab38610801aafbc0e93b1828a4bcd
bbFactory <borahm87@gmail.com>
2021-11-01 13:41:31 +0000

    Feat : ShoppingmallAdmin 추가

--- server/shoppingmall/admin.py
+++ server/shoppingmall/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from server.common.models import ShoppingMall
 
-# Register your models here.
+
+class ShoppingMallAdmin(admin.ModelAdmin):
+    list_display = ("id", "image", "brand", "url", "price",)
+
+
+admin.site.register(ShoppingMall, ShoppingMallAdmin)
