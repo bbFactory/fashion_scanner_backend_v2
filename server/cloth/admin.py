48988c9885df2b030c4e594341307925a2ce949f
HyunAm0225 <41044429+HyunAm0225@users.noreply.github.com>
2021-11-01 23:01:50 +0900

    Feat : cloth admin 추가 (#9)

--- server/cloth/admin.py
+++ server/cloth/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from server.cloth.models import Clothes, Category, Attribute
+from django.utils.html import format_html
 
 # Register your models here.
+@admin.register(Clothes)
+class ClothesAdmin(admin.ModelAdmin):
+    def product_cover_thumbnail(self, obj):
+        return format_html('<img src="{}" width="50px;"/>'.format(obj.thumnail))
+
+    product_cover_thumbnail.short_description = "Thumbnail"
+    list_display = (
+        "id",
+        "product_cover_thumbnail",
+        "image",
+        "member",
+        "color",
+        "category",
+        # "attributes",
+    )
