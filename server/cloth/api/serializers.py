2026a3a49d016c0ec164c345d50ace82307d7083
bbFactory <borahm87@gmail.com>
2021-11-21 12:00:02 +0000

    Refact : 한/영 데이터 구조 변경

--- server/cloth/api/serializers.py
+++ server/cloth/api/serializers.py
@@ -10,30 +10,17 @@ class AttributeSerializer(serializers.ModelSerializer):
         fields = ["ko_name"]
 
 
-class ClothSerializer(serializers.ModelSerializer):
-    color_name = serializers.SerializerMethodField("get_color_name")
-    ko_category_name = serializers.SerializerMethodField("get_category_name")
-    en_category_name = serializers.SerializerMethodField("get_en_category_name")
+class KoClothSerializer(serializers.ModelSerializer):
+    category_name = serializers.SerializerMethodField("get_category_name")
     attributes = serializers.SerializerMethodField("get_attribute_name")
-    en_attributes = serializers.SerializerMethodField("get_en_attribute_name")
 
     class Meta:
         model = Clothes
         fields = [
-            "id",
             "image",
-            "color_name",
-            # "category_id",
-            "ko_category_name",
-            "en_category_name",
-            "attributes",
-            "en_attributes",
-            # "ko_attribute",
-            # "en_attribute_list",
+            "category_name",
+            "attributes"
         ]
-
-    def get_color_name(self, obj):
-        return obj.color.hex_code
 
     def get_category_name(self, obj):
         return obj.category.ko_name
@@ -41,8 +28,21 @@ class ClothSerializer(serializers.ModelSerializer):
     def get_attribute_name(self, obj):
         return [attribute.ko_name for attribute in obj.attributes.all()]
 
+
+class EnClothSerializer(serializers.ModelSerializer):
+    category_name = serializers.SerializerMethodField("get_en_category_name")
+    attributes = serializers.SerializerMethodField("get_en_attribute_name")
+
+    class Meta:
+        model = Clothes
+        fields = [
+            "image",
+            "category_name",
+            "attributes",
+        ]
+
     def get_en_category_name(self, obj):
         return obj.category.en_name
 
     def get_en_attribute_name(self, obj):
-        return [attribute.en_name for attribute in obj.attributes.all()]
+        return [attribute.en_name.replace("_", " ").title() for attribute in obj.attributes.all()]
--- server/member/api/serializers.py
+++ server/member/api/serializers.py
@@ -6,12 +6,39 @@ from rest_framework import serializers
 from server.member.models import Members
 
 
-class MemberSerialzier(serializers.ModelSerializer):
+class KoMemberSerialzier(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField("get_ko_name")
+    group_type = serializers.SerializerMethodField("get_group_type")
+
     class Meta:
         model = Members
         fields = [
-            "id",
-            "ko_name",
-            "en_name",
+            "name",
             "group_type",
         ]
+    
+    def get_ko_name(self, obj):
+        return obj.ko_name
+
+    def get_group_type(self, obj):
+        return obj.group_type.capitalize()
+
+
+class EnMemberSerialzier(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField("get_en_name")
+    group_type = serializers.SerializerMethodField("get_group_type")
+    
+    class Meta:
+        model = Members
+        fields = [
+            "name",
+            "group_type",
+        ]
+    
+    def get_en_name(self, obj):
+        return obj.en_name.capitalize()
+    
+    def get_group_type(self, obj):
+        return obj.group_type.capitalize()
+
+    
